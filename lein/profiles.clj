10534c03235c3f5ed2d623904b194a7aaecec4fd
Dave Aitken <dave.aitken@gmail.com>
2014-10-23 20:57:31 +0100

    Add some lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.7.0"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.7.0"]
+                  [lein-kibit "0.0.8"]
+                  [jonase/eastwood "0.1.4"]
+                  [lein-ancient "0.5.5"]]}}
