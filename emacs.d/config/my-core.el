b08a259c1f548dd891b990ca86e2273cf4c83b1c
Dave Aitken <dave.aitken@gmail.com>
2014-12-12 16:57:59 +0000

    Some tweaks

--- emacs.d/config/my-core.el
+++ emacs.d/config/my-core.el
@@ -1,18 +1,17 @@
 ;; Disable toolbars and splash screens.
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-
-;; Disable the menu bar in console emacs.
 (unless (display-graphic-p) (menu-bar-mode -1))
-
-(setq require-final-newline t)
-(setq-default indent-tabs-mode nil)
 
 (defvar my-auto-save-folder "~/.emacs.d/.saves/"
   "Directory used for Emacs backups.")
+;; Disable the menu bar in console emacs.
 
-(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
-(setq auto-save-file-name-transforms
-      `((".*" ,my-auto-save-folder t)))
+(setq backup-directory-alist `(("." . ,my-auto-save-folder)))
+(setq auto-save-file-name-transforms `((".*" ,my-auto-save-folder t)))
+(setq create-lockfiles nil)
+
+(setq require-final-newline t)
+(setq-default indent-tabs-mode nil)
 
 (load-theme 'solarized-dark t)
 (provide 'my-core)
