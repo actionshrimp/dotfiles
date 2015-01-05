4568160e232affe1aa9ec0af53dbf5b92fc9b944
Dave Aitken <dave.aitken@gmail.com>
2015-01-05 08:03:03 +0000

    Add jsx support (checker not working?)

--- emacs.d/config/languages/my-javascript.el
+++ emacs.d/config/languages/my-javascript.el
@@ -19,4 +19,23 @@
      :next-checkers (javascript-jshint))
 
 (add-hook 'js3-mode-hook 'enable-common-lang)
+(add-hook 'js-mode-hook 'enable-common-lang)
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+(flycheck-define-checker jsx-jsxhint
+  "A JSX syntax and style checker based on JSXHint."
+
+  :command ("jsxhint" source)
+  :error-patterns
+  ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
+  :modes (web-mode))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (equal web-mode-content-type "jsx")
+              ;; enable flycheck
+              (flycheck-select-checker 'jsxhint-checker))))
+
+
 (provide 'my-javascript)
--- emacs.d/config/my-packages.el
+++ emacs.d/config/my-packages.el
@@ -12,6 +12,10 @@
   :ensure flycheck
   :commands global-flycheck-mode)
 
+(use-package web-mode
+  :ensure web-mode)
+(add-hook 'web-mode-hook 'enable-common-lang)
+
 ;Requires installation of editorconfig-core!
 (use-package editorconfig
   :ensure editorconfig
