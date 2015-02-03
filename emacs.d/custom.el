3b7960fab36cb0fe6cf7b6cee9f2ab60613d1d70
Dave Aitken <dave.aitken@gmail.com>
2015-02-03 17:07:14 +0000

    Some small custom tweaks

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    (quote
     ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "b71d5d49d0b9611c0afce5c6237aacab4f1775b74e513d8ba36ab67dfab35e5a" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(exec-path-from-shell-variables (quote ("PATH" "MANPATH")))
  '(flycheck-checkers (quote (javascript-jscs javascript-jshint)))
  '(global-flycheck-mode t nil (flycheck))
  '(indent-tabs-mode nil)
@@ -14,7 +15,7 @@
  '(js3-enter-indents-newline t)
  '(js3-global-externs
    (quote
-    ("require" "module" "process" "setTimeout" "setImmediate" "before" "beforeEach" "describe" "it" "afterEach" "after")))
+    ("require" "module" "process" "setTimeout" "setImmediate" "before" "beforeEach" "describe" "it" "afterEach" "after" "__dirname")))
  '(js3-indent-level 4)
  '(js3-indent-on-enter-key t)
  '(js3-mode-show-parse-errors nil)
