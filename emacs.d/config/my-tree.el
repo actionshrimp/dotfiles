5bf53a55173cabea562c6bca4486d0c2af329c55
Dave Aitken <dave.aitken@gmail.com>
2015-02-03 17:06:56 +0000

    Better tree toggling function

--- emacs.d/config/my-tree.el
+++ emacs.d/config/my-tree.el
@@ -8,9 +8,24 @@
   :config
 
   (progn
-    (evil-define-key 'normal global-map (kbd "<f2>") 'tree-in-project-root)
-    (evil-define-key 'emacs global-map (kbd "<f2>") 'tree-in-project-root)
-    (evil-define-key 'normal dirtree-mode-map (kbd "<f2>") 'kill-buffer-and-window))
-  )
+    (evil-define-key 'normal global-map (kbd "<f2>") 'toggle-tree)
+    (evil-define-key 'emacs global-map (kbd "<f2>") 'toggle-tree)
+    (evil-define-key 'normal dirtree-mode-map (kbd "<f2>") 'toggle-tree)
+    (evil-define-key 'normal dirtree-mode-map (kbd "q") 'toggle-tree)
+    (evil-define-key 'normal dirtree-mode-map (kbd "<f5>") 'tree-mode-reflesh)
+    ))
+
+(defun toggle-tree ()
+  (interactive)
+  (let ((killed nil))
+    (save-excursion
+      (dolist (window (window-list))
+        (select-window window)
+        (if (string-equal "*dirtree*" (buffer-name))
+            (progn
+              (kill-buffer-and-window)
+              (setq killed t)))))
+    (unless killed
+      (tree-in-project-root))))
 
 (provide 'my-tree)
