c6c682eb548d0d64cd8678bdc22f7633d1f8d58f
Dave Aitken <dave.aitken@gmail.com>
2015-02-09 20:15:26 +0000

    Updates to haskell config and bindings

--- emacs.d/config/languages/my-haskell.el
+++ emacs.d/config/languages/my-haskell.el
@@ -1,14 +1,38 @@
+(defun haskell-process-completions-at-point ()
+  "A company-mode-compatible complete-at-point function."
+  (interactive)
+  (destructuring-bind (start . end) (bounds-of-thing-at-point 'symbol)
+    (let ((completions
+           (haskell-process-get-repl-completions (haskell-process)
+                                                 (symbol-name (symbol-at-point)))))
+      (list start end completions))))
+
+
+(setq haskell-process-type 'cabal-repl
+      haskell-process-path-cabal "~/.cabal/bin/cabal"
+      flycheck-haskell-hlint-executable "~/.cabal/bin/hlint"
+      )
+
 (use-package haskell-mode
   :ensure haskell-mode
   :mode ("\\.l?hs$" . haskell-mode)
   :config (progn
-            (add-hook 'haskell-mode-hook 'enable-common-lang)
-            (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)))
+            (defun on-haskell-mode ()
+              (enable-common-lang)
+              (turn-on-haskell-simple-indent)
+              (flycheck-haskell-setup)
+              ;(flycheck-select-checker 'haskell-ghc)
+              )
+            (add-hook 'haskell-mode-hook 'on-haskell-mode)
+            (evil-define-key 'normal haskell-mode-map
+              (kbd ",t") 'haskell-process-do-type
+              (kbd ",i") 'haskell-process-do-info
+              (kbd ",;") 'haskell-interactive-switch
+              )
 
-(use-package flycheck-haskell
-  :ensure flycheck-haskell
-  :mode ("\\.l?hs$" . haskell-mode)
-  :config (progn
-            (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)))
+            (evil-set-initial-state 'haskell-interactive-mode 'emacs)
+            (evil-define-key 'emacs haskell-interactive-mode-map
+              (kbd "<RET>") 'haskell-interactive-mode-return)
+            ))
 
 (provide 'my-haskell)
