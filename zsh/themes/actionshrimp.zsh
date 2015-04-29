d38cb1e0bc41c1cefc0d35a0898937b65f64d8f4
Dave Aitken <dave.aitken@gmail.com>
2015-04-29 15:21:36 +0100

    Timestamp after command is run

--- zsh/themes/actionshrimp.zsh
+++ zsh/themes/actionshrimp.zsh
@@ -25,8 +25,38 @@ ZSH_THEME_GIT_PROMPT_CLEAN=""
 
 PROMPT='%{%f%k%b%}
 %{%K{${bkg}}%B%F{green}%}%n%{%B%F{blue}%}@%{%B%F{cyan}%}%m%{%B%F{green}%} %{%b%F{yellow}%K{${bkg}}%}%~%{%B%F{green}%}$(git_prompt_info)%E%{%f%k%b%}
-%{%K{${bkg}}%}[%D{%H:%M:%S}] $(_prompt_char)%{%K{${bkg}}%} Ξ%{%f%k%b%} '
+%{%K{${bkg}}%}$(_prompt_char)%{%K{${bkg}}%} Ξ%{%f%k%b%} '
 
-RPROMPT=''
+#RPROMPT=''
+
+strlen () {
+    FOO=$1
+    local zero='%([BSUbfksu]|([FB]|){*})'
+    LEN=${#${(S%%)FOO//$~zero/}}
+    echo $LEN
+}
+
+# show right prompt with date ONLY when command is executed
+preexec () {
+    DATE=$( date +"[%H:%M:%S]" )
+    local len_right=$( strlen "$DATE" )
+    len_right=$(( $len_right+1 ))
+    local right_start=$(($COLUMNS - $len_right))
+
+    local len_cmd=$( strlen "$@" )
+    local len_prompt=$(strlen "$PROMPT" )
+    local len_left=$(($len_cmd+$len_prompt))
+
+    RDATE="\033[${right_start}C ${DATE}"
+
+    if [ $len_left -lt $right_start ]; then
+        # command does not overwrite right prompt
+        # ok to move up one line
+        echo -e "\033[1A${RDATE}"
+    else
+        echo -e "${RDATE}"
+    fi
+
+}
 
 #RPROMPT='!%{%B%F{cyan}%}%!%{%f%k%b%}'
