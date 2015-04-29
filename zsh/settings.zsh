a84c911b973771c20fa78720bbf29fda3b01deaa
Dave Aitken <dave.aitken@gmail.com>
2015-04-29 15:13:39 +0100

    Dont autostart tmux

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -2,7 +2,7 @@ setopt APPEND_HISTORY
 stty -ixon
 
 COMPLETION_WAITING_DOTS="true"
-export ZSH_TMUX_AUTOSTART=true
+#export ZSH_TMUX_AUTOSTART=true
 export ZSH_TMUX_AUTOQUIT=false
 
 unsetopt listambiguous
