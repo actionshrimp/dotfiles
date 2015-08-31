c1f90637677c19b483b730837f166bc2620c209d
Dave Aitken <dave.aitken@gmail.com>
2015-08-31 20:56:44 +0100

    Upgrade clojure deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,8 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]
                   [lein-kibit "0.0.8"]
                   [jonase/eastwood "0.1.4"]
-                  [lein-ancient "0.5.5"]]}}
+                  [lein-ancient "0.5.5"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.10"]]
+        }}
