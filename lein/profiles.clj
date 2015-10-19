bda9006b8b0cd775c34b87f7445f7e897ef4c07d
Dave Aitken <dave.aitken@gmail.com>
2015-10-19 16:57:23 +0100

    nrepl middleware updates

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.9.1"]
-                  ;[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "1.1.0"]
+{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]
                   [jonase/eastwood "0.1.4"]
                   [lein-kibit "0.0.8"]]
         :dependencies [[alembic "0.3.2"]
