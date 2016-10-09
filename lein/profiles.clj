98515ad9893cae98598e45e09606a47a40efa741
Dave Aitken <dave.aitken@gmail.com>
2016-10-09 13:30:38 +0100

    lein profile bumps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,8 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [jonase/eastwood "0.2.3"]
-                  [lein-kibit "0.1.2"]
+{:user {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
                   [lein-pprint "1.1.1"]
                   [lein-libdir "0.1.1"]
-                  [lein-clean-m2 "0.1.2"]]
+                  [lein-clean-m2 "0.1.2"]
+                  [refactor-nrepl "2.3.0-SNAPSHOT"]]
         :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]]
-        :env {}}}
+                       [org.clojure/tools.nrepl "0.2.12"]
+                       [acyclic/squiggly-clojure "0.1.6"]]}}
