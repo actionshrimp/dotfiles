0f6ca951b11d5b7c6d5b43a742911819262d8fcc
Dave Aitken <dave.aitken@gmail.com>
2017-08-07 21:50:00 +0100

    Sort out lein profile finally

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
-                  [lein-pprint "1.1.1"]
-                  [lein-libdir "0.1.1"]
-                  [lein-clean-m2 "0.1.2"]
+{:user {:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
-        :dependencies [[alembic "0.3.2"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       [acyclic/squiggly-clojure "0.1.6"]]}}
+        :dependencies []
+        :jvm-opts ["-Dapple.awt.UIElement=true" "-XX:-OmitStackTraceInFastThrow"]}}
