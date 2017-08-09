66411da84ea1d87e823aae7f715b3e4378813ce8
Dave Aitken <dave.aitken@gmail.com>
2017-08-09 22:21:03 +0100

    Set JVM timezone to UTC

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]
                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
         :dependencies []
-        :jvm-opts ["-Dapple.awt.UIElement=true" "-XX:-OmitStackTraceInFastThrow"]}}
+        :jvm-opts ["-Dapple.awt.UIElement=true" "-XX:-OmitStackTraceInFastThrow" "-Duser.timezone=UTC"]}}
