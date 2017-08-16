f2d8fae3a80d4630116e5175af849e255d09f87c
Dave Aitken <dave.aitken@gmail.com>
2017-08-16 09:37:50 +0100

    Add template sql config

---
+++ spacemacs.d/layers/my-sql/config.el
@@ -0,0 +1,15 @@
+;; (setq sql-connection-alist
+;;  '(("db-name"
+;;      (sql-product 'postgres)
+;;      (sql-user "user")
+;;      (sql-password "") ;;takes from ~/.pgpass
+;;      (sql-server "localhost")
+;;      (sql-database "dbname")
+;;      (sql-port 5432))
+;;    ))
+
+;;(setq-default sql-product 'postgres)
+;;(setq-default sql-database "user")
+;;(setq-default sql-password "")
+;;(setq-default sql-server "localhost")
+;;(setq-default sql-user "dbname")
