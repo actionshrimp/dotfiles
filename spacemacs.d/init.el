ed4a0d8a935afbf4c1224e93ebd8cb5b0565d55e
Dave Aitken <dave.aitken@gmail.com>
2018-01-14 22:45:34 +0000

    Disable syntax tooltips

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -53,7 +53,7 @@ This function should only modify configuration layer settings."
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
-     syntax-checking
+     (syntax-checking :variables syntax-checking-enable-tooltips nil)
      (version-control :variables
                       version-control-diff-tool 'diff-hl)
      clojure
