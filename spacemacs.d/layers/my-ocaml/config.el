df81ad0af143012a5ccde6520ed5fd2d6971f15a
Dave Aitken <dave.aitken@gmail.com>
2018-01-17 23:05:41 +0000

    Dont comment regions with multi-line in ocaml

--- spacemacs.d/layers/my-ocaml/config.el
+++ spacemacs.d/layers/my-ocaml/config.el
@@ -1,3 +1,7 @@
 ;; Noop to stop tuareg-abbrev-hook error popping up when hitting escape
 (defun tuareg-abbrev-hook ()
   ())
+
+(add-hook 'tuareg-mode-hook
+          (lambda ()
+            (setq-local comment-style 'indent)))
