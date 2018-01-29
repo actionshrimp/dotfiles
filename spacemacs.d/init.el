b5b2d3b78ba0ae71e3a232a39024a3d79c8d82f2
Dave Aitken <dave.aitken@gmail.com>
2018-01-29 21:44:41 +0000

    Linux/mac spacemacs init syncing

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -165,6 +165,15 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(spacemacs-dark)
+
+   ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
+   ;; `all-the-icons', `custom', `vim-powerline' and `vanilla'. The first three
+   ;; are spaceline themes. `vanilla' is default Emacs mode-line. `custom' is a
+   ;; user defined themes, refer to the DOCUMENTATION.org for more info on how
+   ;; to create your own spaceline theme. Value can be a symbol or list with\
+   ;; additional properties.
+   ;; (default '(spacemacs :separator wave :separator-scale 1.5))
+   dotspacemacs-mode-line-theme '(spacemacs :separator wave :separator-scale 1.5)
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
    dotspacemacs-colorize-cursor-according-to-state t
@@ -382,7 +391,8 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
   (eval-after-load 'smartparens
     '(progn
        (sp-pair "\"" nil :actions :rem)
-       (sp-pair "\\\"" nil :actions :rem)))
+       (sp-pair "\\\"" nil :actions :rem)
+       ))
   )
 
 (defun dotspacemacs/user-config ()
