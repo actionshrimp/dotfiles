6ed365cda6361c073c25dabc833d54c20cc5af9b
Dave Aitken <dave.aitken@gmail.com>
2018-01-29 22:09:51 +0000

    Font size based on hostname

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -177,13 +177,6 @@ It should only modify the values of Spacemacs settings."
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    ;; (default t)
    dotspacemacs-colorize-cursor-according-to-state t
-   ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
-   ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Fira Mono"
-                               :size 13
-                               :weight thin
-                               :width normal
-                               :powerline-scale 1.3)
    ;; The leader key (default "SPC")
    dotspacemacs-leader-key "SPC"
    ;; The key used for Emacs commands `M-x' (after pressing on the leader key).
@@ -384,6 +377,9 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
+  ;; Set the Emacs customization file path. Must be done here in user-init.
+  (setq custom-file "~/.spacemacs.d/custom.el")
+
   (setq exec-path-from-shell-check-startup-files nil)
   (with-eval-after-load 'magit
     (magit-add-section-hook 'magit-status-sections-hook 'magit-insert-submodules))
@@ -393,6 +389,15 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
        (sp-pair "\"" nil :actions :rem)
        (sp-pair "\\\"" nil :actions :rem)
        ))
+
+  (print (system-name))
+  (setq-default dotspacemacs-default-font
+                `("Fira Mono"
+                  :size ,(if (string= (system-name) "baracus")
+                             15 13)
+      :weight thin
+                  :width normal
+                  :powerline-scale 1.3))
   )
 
 (defun dotspacemacs/user-config ()
@@ -415,46 +420,11 @@ before packages are loaded."
   (add-to-list 'safe-local-variable-values '(utop-command . "jbuilder utop . -- -emacs"))
 
   (setq magit-rebase-arguments '("--autostash"))
+
+  ;; Lastly, load custom-file (but only if the file exists).
+  (when (file-exists-p custom-file)
+    (load-file custom-file))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
-(defun dotspacemacs/emacs-custom-settings ()
-  "Emacs custom settings.
-This is an auto-generated function, do not modify its content directly, use
-Emacs customize menu instead.
-This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(magit-rebase-arguments (quote ("--autostash")))
- '(package-selected-packages
-   (quote
-    (readline-complete hasky-stack packed org-category-capture imenu-list window-purpose projectile hydra undo-tree powerline highlight avy evil-cleverparens evil-lispy lispyville parinfer lispy zoutline swiper ivy smartparens evil helm helm-core async dash org-brain impatient-mode evil-org solarized-theme yapfify yaml-mode xterm-color web-mode web-beautify toml-mode tagedit sql-indent smeargle slim-mode shell-pop scss-mode sayid sass-mode rvm ruby-tools ruby-test-mode ruby-refactor rubocop rspec-mode robe rbenv rake racer pyvenv pytest pyenv-mode py-isort pug-mode psci purescript-mode psc-ide pip-requirements orgit org-projectile org-present org-pomodoro alert log4e gntp org-download multi-term mmm-mode minitest markdown-toc markdown-mode magit-gitflow livid-mode skewer-mode simple-httpd live-py-mode less-css-mode json-mode json-snatcher json-reformat js2-refactor js2-mode js-doc intero insert-shebang idris-mode prop-menu hy-mode htmlize hlint-refactor hindent helm-pydoc helm-hoogle helm-gitignore helm-css-scss helm-company helm-c-yasnippet haskell-snippets haml-mode godoctor go-rename go-guru go-eldoc gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ git-gutter-fringe fringe-helper git-gutter+ git-gutter gh-md fuzzy flycheck-rust flycheck-pos-tip pos-tip flycheck-haskell flycheck-gometalinter flycheck-elm flycheck-bashate fish-mode evil-smartparens evil-magit magit magit-popup git-commit with-editor eshell-z eshell-prompt-extras esh-help emmet-mode elm-mode diff-hl dante flycheck cython-mode company-web web-completion-data company-tern dash-functional tern company-statistics company-shell company-go go-mode company-ghci company-ghc ghc haskell-mode company-cabal company-anaconda company coffee-mode cmm-mode clojure-snippets clj-refactor inflections edn multiple-cursors paredit peg cider-eval-sexp-fu cider queue clojure-mode chruby cargo rust-mode bundler inf-ruby browse-at-remote auto-yasnippet yasnippet anaconda-mode pythonic ac-ispell auto-complete ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org symon string-inflection spaceline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el password-generator paradox org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint info+ indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-make helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu elisp-slime-nav editorconfig dumb-jump define-word column-enforce-mode clean-aindent-mode auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line)))
- '(safe-local-variable-values
-   (quote
-    ((js2-basic-offset . 2)
-     (js-indent-level . 2)
-     (flycheck-checkers javascript-flow javascript-my-eslint)
-     (eval define-clojure-indent
-           (lazy-seq 0)
-           (match 1)
-           (try+ 0)
-           (pdoseq 2))
-     (cider-refresh-after-fn . "bounce.core/start!")
-     (cider-refresh-before-fn . "bounce.core/stop!")
-     (cider-cljs-lein-repl . "(user/cljs-repl)")
-     (eval tuareg-opam-update-env "/Users/dave/dev/ai/sigmax")
-     (utop-command . "jbuilder utop . -- -emacs")
-     (eval tuareg-opam-update-env "/home/dave/dev/ai/sigmax")
-     (eval pyvenv-activate "/home/dave/dev/ai/test-manager/venv")
-     ))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-)
