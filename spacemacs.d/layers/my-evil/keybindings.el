9d0fa53ce4d62624e8532d94ff8234aeb8dbdf9e
Dave Aitken <dave.aitken@gmail.com>
2018-01-30 23:07:56 +0000

    try out tmux friendly bindings

--- spacemacs.d/layers/my-evil/keybindings.el
+++ spacemacs.d/layers/my-evil/keybindings.el
@@ -3,10 +3,10 @@
 
 (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
-(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
-(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
-(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+;; (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+;; (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+;; (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+;; (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
 ;;(define-key company-active-map (kbd "C-w") 'evil-delete-backward-word)
 (define-key isearch-mode-map (kbd "C-w") 'isearch-exit)
--- spacemacs.d/layers/my-sql/keybindings.el
+++ spacemacs.d/layers/my-sql/keybindings.el
@@ -6,7 +6,7 @@
 (evil-define-key 'normal sql-interactive-mode-map ";" 'evil-repeat-find-char)
 (evil-define-key 'visual sql-interactive-mode-map ";" 'evil-repeat-find-char)
 
-(evil-define-key 'normal sql-interactive-mode-map (kbd "C-h") 'evil-window-left)
-(evil-define-key 'normal sql-interactive-mode-map (kbd "C-j") 'evil-window-down)
-(evil-define-key 'normal sql-interactive-mode-map (kbd "C-k") 'evil-window-up)
-(evil-define-key 'normal sql-interactive-mode-map (kbd "C-l") 'evil-window-right)
+;; (evil-define-key 'normal sql-interactive-mode-map (kbd "C-h") 'evil-window-left)
+;; (evil-define-key 'normal sql-interactive-mode-map (kbd "C-j") 'evil-window-down)
+;; (evil-define-key 'normal sql-interactive-mode-map (kbd "C-k") 'evil-window-up)
+;; (evil-define-key 'normal sql-interactive-mode-map (kbd "C-l") 'evil-window-right)
