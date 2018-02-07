520a68deecddf602a8b1d2c7110e55e87929040f
Dave Aitken <dave.aitken@gmail.com>
2018-02-07 21:19:06 +0000

    Tidy up and fix up SQL config

--- spacemacs.d/layers/my-sql/config.el
+++ spacemacs.d/layers/my-sql/config.el
@@ -1,5 +1,17 @@
-;; Example config. Encrypted db config is loaded in spacemacs/user-config as it requires
-;; some init to be performed first on OSX (not sure what exactly, perhaps exec-path-from-shell?)
+(setq-default sql-product 'mysql)
+(setq-default sql-password "password")
+(setq-default sql-server "127.0.0.1")
+(setq-default sql-user "username")
+(setq-default sql-port 3306)
+
+(with-eval-after-load "sql"
+  (sql-set-product-feature 'mysql :prompt-regexp "^\\(MariaDB\\|MySQL\\) \\[[_a-zA-Z()]*\\]> ")
+  (setq sql-mysql-options (list "--protocol=tcp"))
+  (setq sql-mysql-login-params (append sql-mysql-login-params '(port))))
+
+;; Example connection config. Encrypted db config is loaded in
+;; spacemacs/user-config as it requires some init to be performed first on OSX
+;; (not sure what exactly, perhaps exec-path-from-shell?)
 
 ;; (setq sql-connection-alist
 ;;  '(("db-name"
