1da1fa1c861a3b6fde9a572289ea3f07aba4d94f
Dave Aitken <dave.aitken@gmail.com>
2018-06-11 10:42:22 +0100

    Turn on line numbers

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -312,7 +312,7 @@ It should only modify the values of Spacemacs settings."
    ;;                       text-mode
    ;;   :size-limit-kb 1000)
    ;; (default nil)
-   dotspacemacs-line-numbers nil
+   dotspacemacs-line-numbers t
    ;; Code folding method. Possible values are `evil' and `origami'.
    ;; (default 'evil)
    dotspacemacs-folding-method 'evil
