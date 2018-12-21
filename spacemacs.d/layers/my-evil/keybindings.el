7fda43fb265710cf8d3d95cba3faf0ac4b7be3c7
Dave Aitken <dave.aitken@gmail.com>
2018-12-21 19:13:49 +0000

    finally working C-x C-l line completion a-la vim!

--- spacemacs.d/layers/my-evil/keybindings.el
+++ spacemacs.d/layers/my-evil/keybindings.el
@@ -21,10 +21,4 @@
                              helm-grep-map))
               (define-key m (kbd "C-w") 'backward-kill-word))))
 
-(defun my-expand-lines ()
-  (interactive)
-  (let ((hippie-expand-try-functions-list
-         '(try-expand-line-all-buffers)))
-    (call-interactively 'hippie-expand)))
-
-(define-key evil-insert-state-map (kbd "C-x C-l") 'my-expand-lines)
+(define-key evil-insert-state-map (kbd "C-x C-l") 'company-hippie-line)
--- spacemacs.d/layers/my-evil/packages.el
+++ spacemacs.d/layers/my-evil/packages.el
@@ -58,5 +58,104 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
+(require 'hippie-exp)
+(require 'cl-lib)
+
+(defun my-try-expand-line-all-buffers (old)
+  "Try to complete the current line, searching all other buffers.
+The argument OLD has to be nil the first call of this function, and t
+for subsequent calls (for further possible completions of the same
+string).  It returns t if a new completion is found, nil otherwise."
+  (my-he--all-buffers
+   old
+   (lambda () (he-line-beg (and (get-buffer-process (current-buffer))
+                                comint-use-prompt-regexp
+                                comint-prompt-regexp)))
+   (lambda (string)
+     (he-line-search string
+                     (and (get-buffer-process (current-buffer))
+                          comint-use-prompt-regexp
+                          comint-prompt-regexp)
+                     nil))))
+
+(defun my-he--all-buffers (old beg-function search-function)
+  (let ((expansion ())
+        (buf (current-buffer))
+        (only-buffers hippie-expand-only-buffers)
+        (ignore-buffers hippie-expand-ignore-buffers)
+        (orig-case-fold-search case-fold-search))
+    (if (not old)
+        (progn
+	  (he-init-string (funcall beg-function) (point))
+	  (setq he-search-bufs (buffer-list))
+	  (setq he-searched-n-bufs 0)
+	  (set-marker he-search-loc 1 (car he-search-bufs))))
+
+    (if (not (equal he-search-string ""))
+	(while (and he-search-bufs
+		    (not expansion)
+		    (or (not hippie-expand-max-buffers)
+                        (< he-searched-n-bufs hippie-expand-max-buffers)))
+          (set-buffer (car he-search-bufs))
+          (if (and (not (eq (current-buffer) buf))
+                   (if only-buffers
+                       (he-buffer-member only-buffers)
+                     (not (he-buffer-member ignore-buffers))))
+              (save-excursion
+                (save-restriction
+                  (if hippie-expand-no-restriction
+		      (widen))
+		  (goto-char he-search-loc)
+		  (setq expansion
+			(let ((case-fold-search orig-case-fold-search))
+			  (funcall search-function he-search-string)))
+		  (set-marker he-search-loc (point))
+		  (if (not expansion)
+		      (progn
+			(setq he-search-bufs (cdr he-search-bufs))
+			(setq he-searched-n-bufs (1+ he-searched-n-bufs))
+			(set-marker he-search-loc 1 (car he-search-bufs))))))
+	    (setq he-search-bufs (cdr he-search-bufs))
+	    (set-marker he-search-loc 1 (car he-search-bufs)))))
+
+    (set-buffer buf)
+    (if (not expansion)
+	(progn
+	  (if old (he-reset-string))
+	  ())
+      (progn
+        expansion
+        ))))
+
+(defun get-hippie-expand-lines ()
+  (let (completions-list candidate)
+    (setq candidate (my-try-expand-line-all-buffers nil))
+    (if candidate
+        (progn
+          (push candidate completions-list)
+          (while (progn
+                   (setq candidate (my-try-expand-line-all-buffers t))
+                   (if candidate
+                       (push candidate completions-list)
+                     nil))))
+      nil)
+    completions-list))
+
+(defun company-hippie-line (command &optional arg &rest ignored)
+  (interactive (list 'interactive))
+
+  (cl-case command
+    (interactive (company-begin-backend 'company-hippie-line))
+    (prefix (and (company-grab-symbol)
+                 (not (looking-at "[:word:]"))
+                 (let (p1 p2)
+                  (save-excursion
+                    (end-of-line)
+                    (setq p2 (point))
+                    (back-to-indentation)
+                    (setq p1 (point)))
+                  (buffer-substring-no-properties p1 p2))))
+    (candidates (get-hippie-expand-lines))))
+
 
 ;;; packages.el ends here
