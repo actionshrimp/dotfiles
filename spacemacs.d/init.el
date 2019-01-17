05ef773b7e30c534dbedce896dd8c123c029443e
Dave Aitken <dave.aitken@gmail.com>
2019-01-17 21:14:39 +0000

    spacemacs: add xclipboard layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -97,6 +97,7 @@ This function should only modify configuration layer settings."
 
      reason
      imandra
+     xclipboard
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
