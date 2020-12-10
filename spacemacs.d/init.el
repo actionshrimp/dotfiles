18cc4e61982048b7f3413364f833361fee61e146
Dave Aitken <dave.aitken@gmail.com>
2020-12-10 11:24:59 +0000

    disable merlin-eldoc for speeds

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -113,7 +113,7 @@ This function should only modify configuration layer settings."
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '()
+   dotspacemacs-excluded-packages '(merlin-eldoc)
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
    ;; `used-only' installs only explicitly used packages and deletes any unused
