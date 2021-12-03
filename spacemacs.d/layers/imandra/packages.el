1670fd5fa18bdc3298b08f528617d061b604299f
Dave Aitken <dave.aitken@gmail.com>
2021-12-03 17:17:59 +0000

    tuareg for iml always

--- spacemacs.d/layers/imandra/packages.el
+++ spacemacs.d/layers/imandra/packages.el
@@ -58,7 +58,6 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
-(with-eval-after-load 'tuareg
-  (add-to-list 'auto-mode-alist '("\\.iml\\'" . tuareg-mode)))
+(add-to-list 'auto-mode-alist '("\\.iml\\'" . tuareg-mode))
 
 ;;; packages.el ends here
