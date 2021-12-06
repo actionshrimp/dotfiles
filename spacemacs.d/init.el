791a5de65737706c7c0e65089ac4cf66b69b60b3
Dave Aitken <dave.aitken@gmail.com>
2021-12-06 10:36:47 +0000

    load calc in org-babel

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -523,6 +523,8 @@ before packages are loaded."
 
   (setq projectile-switch-project-action 'projectile-dired)
 
+  (add-to-list 'org-babel-load-languages '(calc . t))
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
